d7206b67d3466e3e40a4e3490494388a2fbe4fc4
echu-nan <ester.sosa@nan-labs.com>
2020-12-03 13:21:22 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import ChiefOfProject from "./containers/chief-of-project";
 import ResponsibleOfProtocol from "./containers/responsible-of-protocol";
 import ChiefResult from "./containers/chief-result";
 import ListOfProtocols from "./containers/protocols-list";
-<<<<<<< HEAD
 import Monitoring from "./containers/Monitoring";
-=======
 import NotFoundPage from "./containers/not-found";
->>>>>>> 212249b1c6c67dda5c2347bc9eef8bbef7917236
 
 class App extends React.Component {
   render() {
@@ -21,12 +18,8 @@ class App extends React.Component {
           <Route path="/protocolexec" component={ResponsibleOfProtocol} />
           <Route path="/projects/monitoring" component={Monitoring} />
           <Route path="/result" component={ChiefResult} />
-<<<<<<< HEAD
           <Route path="/projects/:id/protocols" component={ListOfProtocols} />
-=======
-          <Route path="/protocols/:id" component={ListOfProtocols} />
           <Route path="/*" component={NotFoundPage} />
->>>>>>> 212249b1c6c67dda5c2347bc9eef8bbef7917236
         </Switch>
       </Router>
     );
